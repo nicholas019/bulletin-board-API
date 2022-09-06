cd8e7e3b5b34e6a8f84b64b7c8000e5d96a05bfd
sorkihg <sorkihg@gmail.com>
2022-09-07 01:30:37 +0900

    Feat : Borad app ListView, CreateView API 구현
    1. ListView 구현완료
    2. CreateView 구현완료
     - 비밀번호 유효성검사 구현 : 1개이상숫자와 1개이상의 문자 조합, 최소
       6자
     - 게시글저장시 비밀번호 암호화(bcrypt 라이브러리로 단방향 암호화진행)

--- apps/boards/serialiazers.py
+++ apps/boards/serialiazers.py
@@ -1,10 +1,37 @@
+import bcrypt, re
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from apps.boards.models import Board
 
 
-class BoardSerializer(serializers.Serializer):
-    
+class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
-        fields = "__all__"
+        fields = ("pk", "title", "content", "writer", "password","created_at")
+        
+    def validate(self, attrs):
+        """
+        비밀번호 유효성 검사
+        조건 : 1개이상의 숫자와 1개 이상의 문자가 들어가는 최소 6자 이상 비밀번호 조합
+        """
+        password = attrs['password']
+        REGEX_PASSWORD = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$"
+        if not re.match(REGEX_PASSWORD, password):
+            raise ValidationError( '비밀번호의 양식이 맞지 않습니다')
+        return attrs
+
+    def create(self, validated_date):
+        """
+        게시글 저장 
+        주요기능 : brcypt 라이브러리로 비밀번호 단방향 암호화 하여 저장
+        """
+        password = validated_date["password"]
+        hash_pw = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+        
+        return Board.objects.create(
+                title    = validated_date["title"],
+                content  = validated_date["content"],
+                writer   = validated_date["writer"],
+                password = hash_pw
+                )
--- apps/boards/views.py
+++ apps/boards/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import bcrypt
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from apps.boards.models import Board
 
-# Create your views here.
+from apps.boards.serialiazers import BoardSerializer
+
+# 게시글 리스트와 저장 API
+class BoardListCreateView(APIView):
+    def get(self, request):
+        board = Board.objects.all()
+        serializer = BoardSerializer(board, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = BoardSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)  
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
