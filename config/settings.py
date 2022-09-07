c20e8c15efb4d4328fcf725c40050cd816adef71
sorkihg <sorkihg@gmail.com>
2022-09-07 22:56:20 +0900

    Feat : 추가구현 : Test code 8종 구현 및 오타 수정
    - APITestCase 클래스를 활용하여 Test Code구현
    - 게시판 리스트 조회 성공
    - 게시판 상세 페이지 조회 성공
    - 게시판 글 작성 성공 및 실패(비밀번호 조건 부합)
    - 게시판 글 업데이트 성공 및 실패(비밀번호 오류)
    - 게시판 글 삭제 성공 및 실패(비밀번호 오류)

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ import pymysql
 
 from pathlib import Path
 
-from my_settings import SECRET_KEY, DATABASES, WEATHER_API_Key
+from my_settings import SECRET_KEY, DATABASES, WEATHER_API_KEY
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,7 +13,7 @@ SECRET_KEY = SECRET_KEY
 
 DATABASES = DATABASES
 
-WEATHER_API_Key = WEATHER_API_Key
+WEATHER_API_KEY = WEATHER_API_KEY
 #
 
 DEBUG = True
